f7417ca264fbda47c0aaadb9102a013030eba357
Rachit Shah <shahrachit1299@gmail.com>
2019-02-09 20:48:53 +0530

    after chang in profile

--- app/src/main/java/com/example/rachitshah/edit_profile/MainActivity.java
+++ app/src/main/java/com/example/rachitshah/edit_profile/MainActivity.java
@@ -1,13 +1,47 @@
 package com.example.rachitshah.edit_profile;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView img;
+    private static final int PICK_IMAGE = 100;
+    Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        img = (ImageView) findViewById(R.id.profile);
+        img = (ImageView) findViewById(R.id.profile);
+        img.setOnClickListener(new View.OnClickListener() {
+                                   @Override
+                                   public void onClick(View view) {
+                                       opengallery();
+                                   }
+                               }
+        );
+
+
+    }
+
+    private void opengallery() {
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == PICK_IMAGE) {
+            imageUri = data.getData();
+            img.setImageURI(imageUri);
+        }
     }
 }
